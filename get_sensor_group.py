eee819875890c62a060a6b2d23b589ca5e0ffe52
Yu-Ru Huang <mary@linshunghuang.com>
2015-08-31 17:59:34 +0800

    more

--- get_sensor_group.py
+++ get_sensor_group.py
@@ -8,10 +8,20 @@ import re
 
 
 def get_sensor_group(sensor_variable):
-    group_string=re.search('^\w+.',sensor_variable)
+    group_string=re.search('^\w+\.',sensor_variable)
     that_group=group_string.group(0)
     that_group=that_group.replace(".","")
     return that_group
-    
+
+
+def get_sensor_group(sensor_variable):
+    group_string=re.search('(?<!.)\w+',sensor_variable)
+    return group_string.group(0)
+def get_sensor_group(sensor_variable):
+    group_string=re.search('^\w+',sensor_variable)
+    return group_string.group(0)
+  
+        
 #####example    
 get_sensor_group('TMAl_1.source')
+get_sensor_group('RunHydride_2')
