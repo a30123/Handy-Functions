e7454ed003e8025bb0f71f540f46d01f0ff9b4ec
Yu-Ru Huang <mary@linshunghuang.com>
2016-02-20 23:35:50 +0800

    space

--- extract_serial_number_ultimate.py
+++ extract_serial_number_ultimate.py
@@ -13,10 +13,10 @@ def extract_serial_number_ultimate(filename,tail_string):
     value_of_number=int(serial_number_string)
     return value_of_number 
     
-def extract_serial_number_ultimate(filename,tail_string="-current.csv"):
-    extract_regular_expression=re.search('(_\d+'+tail_string+'$)',filename)
-    serial_number_string=extract_regular_expression.group(0)
-    serial_number_string=serial_number_string.replace(tail_string,'')
-    serial_number_string=serial_number_string.replace('_','') 
-    value_of_number=int(serial_number_string)
+def extract_serial_number_ultimate(filename, tail_string="-current.csv"):
+    extract_regular_expression = re.search('(_\d+'+tail_string+'$)',filename)
+    serial_number_string = extract_regular_expression.group(0)
+    serial_number_string = serial_number_string.replace(tail_string,'')
+    serial_number_string = serial_number_string.replace('_','') 
+    value_of_number = int(serial_number_string)
     return value_of_number 
